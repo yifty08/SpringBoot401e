0efff97075e9e86a90e9a7262677436592c867f8
Yifty <yifty08@icloud.com>
2019-05-28 10:36:49 -0400

    basic security

---
+++ src/main/java/com/example/spring401e/Spring401eApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring401e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring401eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring401eApplication.class, args);
+	}
+
+}
