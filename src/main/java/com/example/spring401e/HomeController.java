699d2119605951c84caef762ac2298b4cddc4ea6
Yifty <yifty08@icloud.com>
2019-06-06 15:35:33 -0400

    updating heroku

--- src/main/java/com/example/spring401e/HomeController.java
+++ src/main/java/com/example/spring401e/HomeController.java
@@ -2,6 +2,7 @@ package com.example.spring401e;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.Arrays;
 
 @Controller
 public class HomeController {
@@ -60,5 +63,33 @@ public class HomeController {
                 ).getUser();
         model.addAttribute("myuser", myuser);
         return "secure";
+    }
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void fillTable() {
+        roleRepository.save(new Role("USER"));
+        roleRepository.save(new Role("ADMIN"));
+
+        Role adminRole = roleRepository.findByRole("ADMIN");
+        Role userRole = roleRepository.findByRole("USER");
+
+        User user = new User("jim@jim.com", "password", "Jim",
+                "Jimmerson", true, "jim");
+        user.setRoles(Arrays.asList(userRole));
+        userRepository.save(user);
+
+        user = new User("admin@admin.com", "password", "Admin",
+                "User", true, "admin");
+        user.setRoles(Arrays.asList(adminRole));
+        userRepository.save(user);
     }
 }
